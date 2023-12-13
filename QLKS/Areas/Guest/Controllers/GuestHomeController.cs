576a5c21d81355ccba9bcbc41d9ac4e6236c0b7c
DUY KHANG <DUY KHANG@MSI>
2023-12-13 11:16:02 +0700

    Add project files.

---
+++ QLKS/Areas/Guest/Controllers/GuestHomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLKS.Areas.Guest.Controllers
+{
+    public class GuestHomeController : Controller
+    {
+        //
+        // GET: /Guest/GuestHome/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
